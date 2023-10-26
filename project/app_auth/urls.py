0319f30a993356198cb4e95f3e5e86c40a5c9ddc
Folkelnstein <143565402+Folkelnstein@users.noreply.github.com>
2023-10-26 17:55:19 +0700

    после нескольких исправлений

--- project/app_auth/urls.py
+++ project/app_auth/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import register, profile, login
+from .views import *
 
 urlpatterns=[
-path('register/', register, name='register'),
-    path('login/', login, name='login'),
+path('register/', register_view, name='register'),
+    path('login/', login_view, name='login'),
     path('profile/', profile, name='profile'),
 ]
--- project/app_auth/views.py
+++ project/app_auth/views.py
@@ -2,19 +2,19 @@ from django.shortcuts import render,redirect,reverse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
-
 from .forms import CustomUserCreationForm
 
 def login(request):
     return render(request, 'app_auth/login.html')
 
 
-def register(request):
+def register_view(request):
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            login(request)
+            user = authenticate(username=user.username, password=request.POST['password1'])
+            login(request, user=user)
             return redirect('profile')
     else:
         form = CustomUserCreationForm()
@@ -29,7 +29,7 @@ def login_view(request):
             return render(request, 'app_auth/login.html')
     username = request.POST['username']
     password = request.POST['password']
-    user = authenticate(request, username=username,password=password)
+    user = authenticate(request, username=username, password=password)
     if user is not None:
         login(request,user)
         return redirect(redirect_url)
